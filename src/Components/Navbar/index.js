edb17471fd8cef4f34cea02317ebf4cd54cd182a
Joaquin Palacios <joa_palacios@hotmail.com>
2021-01-25 13:24:44 +1000

    Desafio 02

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -6,11 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const NavBar = () => (
   <>
     <NavbarBootstrap bg="light" variant="light">
-        <NavbarBootstrap.Brand href="/">WoodLand Furniture</NavbarBootstrap.Brand>
+        <NavbarBootstrap.Brand href="/">Global Bridge</NavbarBootstrap.Brand>
         <Nav className="ml-auto">
-          <Nav.Link className="mx-3" href="/aboutUs">About us</Nav.Link>
-          <Nav.Link className="mx-3" href="/Furniture">Products</Nav.Link>
-          <Nav.Link className="mx-3" href="/location">Location</Nav.Link>
+          <Nav.Link className="mx-3" href="/GlobalBridge">Global Bridge</Nav.Link>
+          <Nav.Link className="mx-3" href="/Aimara">Aimara</Nav.Link>
+          <Nav.Link className="mx-3" href="/CleverCat">Clever Cat</Nav.Link>
+          <Nav.Link className="mx-3" href="/Contact">Contact</Nav.Link>
+
         </Nav> 
     </NavbarBootstrap>
   </>
