3e559e2b247bf3862c62ebb9c8fac38e14db76a5
Joaquin Palacios <joa_palacios@hotmail.com>
2021-01-26 17:09:38 +1000

    Desafio 03 primera parte

---
+++ src/Components/CartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+import Logo from "../CartWidget/Logo.png"
+
+const CartWidgetComponent = () => (
+    <>
+        <img src={Logo} alt='website logo' className="mx-2"/>
+    </>
+);
+
+export default CartWidgetComponent;
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Navbar as NavbarBootstrap, Nav } from "react-bootstrap";
+import CartWidgetComponent from "../CartWidget/CartWidget";
 
 
 const NavBar = () => (
   <>
     <NavbarBootstrap bg="light" variant="light">
-        <NavbarBootstrap.Brand href="/">Global Bridge</NavbarBootstrap.Brand>
+        
+        <NavbarBootstrap.Brand href="/" className="mx-4"><CartWidgetComponent/> Global Bridge</NavbarBootstrap.Brand>
         <Nav className="ml-auto">
-          <Nav.Link className="mx-3" href="/GlobalBridge">Global Bridge</Nav.Link>
+          <Nav.Link className="mx-3" href="/">Global Bridge</Nav.Link>
           <Nav.Link className="mx-3" href="/Aimara">Aimara</Nav.Link>
           <Nav.Link className="mx-3" href="/CleverCat">Clever Cat</Nav.Link>
           <Nav.Link className="mx-3" href="/Contact">Contact</Nav.Link>
