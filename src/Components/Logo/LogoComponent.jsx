950978bbd644f3c3a71273b74166a483674c18d7
Joaquin Palacios <joa_palacios@hotmail.com>
2021-01-27 13:49:29 +1000

    Desafio 03 update1

---
+++ src/Components/Logo/LogoComponent.jsx
@@ -0,0 +1,9 @@
+import Logo from "../Logo/Logo.png"
+
+const LogoComponent = () => (
+    <>
+        <img src={Logo} alt='website logo' className="mx-2"/>
+    </>
+);
+
+export default LogoComponent;
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Navbar as NavbarBootstrap, Nav } from "react-bootstrap";
-import CartWidgetComponent from "../CartWidget/CartWidget";
+import CartWidgetComponet from "../CartWidget/CartWidget";
+import LogoComponent from "../Logo/LogoComponent";
 
 
 const NavBar = () => (
   <>
     <NavbarBootstrap bg="light" variant="light">
         
-        <NavbarBootstrap.Brand href="/" className="mx-4"><CartWidgetComponent/> Global Bridge</NavbarBootstrap.Brand>
+        <NavbarBootstrap.Brand href="/" className="mx-4"><LogoComponent/> Global Bridge</NavbarBootstrap.Brand>
         <Nav className="ml-auto">
           <Nav.Link className="mx-3" href="/">Global Bridge</Nav.Link>
           <Nav.Link className="mx-3" href="/Aimara">Aimara</Nav.Link>
           <Nav.Link className="mx-3" href="/CleverCat">Clever Cat</Nav.Link>
           <Nav.Link className="mx-3" href="/Contact">Contact</Nav.Link>
-
-        </Nav> 
+        </Nav>
+        <Nav.Link><CartWidgetComponet /></Nav.Link>
     </NavbarBootstrap>
   </>
 );
