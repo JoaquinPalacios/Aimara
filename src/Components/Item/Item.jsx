9057fe704591774956ae310cf1221c04da349e5c
Joaquin Palacios <joa_palacios@hotmail.com>
2021-02-03 10:12:08 +1000

    Desafio 05

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,11 +6,11 @@ const Item = ({ varietals }) => {
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={varietals.pictureUrl} />
             <Card.Body>
-                <Card.Title>{varietals.name}</Card.Title>
+                <Card.Title>{varietals.title}</Card.Title>
                 <Card.Text>
                   {varietals.description}
                 </Card.Text>
-                <Button variant="primary">Details</Button>
+                <Button variant="info">Details</Button>
             </Card.Body>
         </Card>
     </>
