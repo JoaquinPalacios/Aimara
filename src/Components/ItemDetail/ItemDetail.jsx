0e2829458bbe1fe2c0f4e646b7c4a95f71d95f27
Joaquin Palacios <joa_palacios@hotmail.com>
2021-02-07 01:37:38 +1000

    desafio 06

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -2,16 +2,16 @@ import { Button } from "react-bootstrap";
 import Item from "../Item/Item";
 import ItemCountComponent from "../ItemCount";
 
-const ItemDetail = ({ varietal }) => {
+const ItemDetail = ({ varietals }) => {
     return (
     
         <div>
-            <Item varietal={varietal} />
-                <div>
+            <Item varietals={varietals} />
+                {/* <div> */}
                     <ItemCountComponent stock={5} initial={1} />
                     <Button>Buy</Button>
                              
-                </div>                  
+                {/* </div>                   */}
         </div>
     )
 };
