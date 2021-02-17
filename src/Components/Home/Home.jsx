1c77596ab4603f6186a5dde634ec4e92df4d343a
Joaquin Palacios <joa_palacios@hotmail.com>
2021-02-17 15:03:31 +1000

    update styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,11 +11,23 @@ const HomeComponent = () => {
               <h1 className="mb-3">
                   Aimara
               </h1>              
-              <p className="mb-5">
+              <p className="lead mb-5">
               Argentina has built a reputation of not only high quality, but also of the unique link wine producers make between their favourite protein and their favourite grape variety: The Malbec.
 With more than two centuries of history, Aimara produce their wines in the region of San Rafael, Mendoza.
               </p>
           </Container>
+          <br />
+          <br />
+          <h5 class="blockquote-footer text-center font-italic">
+              Enjoying fine food and wine at the family table, surrounded by your love ones and friends, is not just a joy. It's one of the highest forms of living.
+              <cite title="Source Title"> Robert Mondavi</cite>
+          </h5>
+          <br />
+          <br />
+          <br />
+          <h1 className="my-5 text-center text-muted">
+              Varietals
+          </h1>
         </>
     );
 };
