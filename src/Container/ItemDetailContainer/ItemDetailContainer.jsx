23f261c769aa51d822685614dcfcfcd0c048f5d1
Joaquin Palacios <joa_palacios@hotmail.com>
2021-04-07 07:39:48 +1000

    update

--- src/Container/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Container/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import Aimara from "../../Aimara";
-import ItemDetail from "../ItemDetail/ItemDetail";
-import { useParams  } from "react-router-dom";
-
-const datos = (id) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(Aimara.find((product) => product.id===parseInt(id)));
-        },3000);
-    });
-};
-const ItemDetailContainer = () => {      
-    const { id } = useParams();
-    const [loading, setLoading] = useState(true);
-    const [varietals, setVarietals] = useState(null);
-    useEffect(() => {
-        datos(id).then((product) => {
-            setVarietals(product);
-        setLoading(false);
-      });
-    }, []);   
-    
-    return (
-        <>
-           {loading ? <div>loading...</div>
-            : <ItemDetail varietals={varietals}  />
-            }
-        </>
-    );           
-};  
-
-export default ItemDetailContainer;
-
-=======
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../../Components/ItemDetail/ItemDetail";
@@ -60,4 +24,3 @@ const ItemDetailContainer = () => {
 };  
 
 export default ItemDetailContainer;
->>>>>>> dev
